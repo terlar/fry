46c8311820d48e3974552679880ab20922259a42
Terje Larsen <terlar@gmail.com>
2013-03-18 21:31:57 +0100

    No need to set universal variable when defaulting

--- share/fry/fry.fish
+++ share/fry/fry.fish
@@ -1,6 +1,6 @@
 # Set defaults
 set -q fry_rubies       ; or set -gx fry_rubies $HOME/.rubies
-set -q fry_auto_switch  ; or set -U fry_auto_switch 0
+set -q fry_auto_switch  ; or set -gx fry_auto_switch 0
 
 # Path
 set -l fry_path (dirname (status -f))
