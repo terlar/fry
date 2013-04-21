90b1287388f59532cf0a51d040a415cdccde0a65
Terje Larsen <terlar@gmail.com>
2013-04-21 12:53:56 +0200

    No need to export variables

--- test/helper.fish
+++ test/helper.fish
@@ -1,4 +1,4 @@
-set -xg fish_tank /usr/local/share/fish-tank/tank.fish
+set fish_tank /usr/local/share/fish-tank/tank.fish
 if not test -e $fish_tank
   set -e fish_tank
   echo 'error: fish-tank is required to run these tests (https://github.com/terlar/fish-tank)'
@@ -7,9 +7,9 @@ end
 
 set -l spec_path (dirname (status -f))
 
-set -xg test_path $PATH
-set -xg fry_rubies $spec_path/dummy-rubies
-set -xg fry_auto_switch 0
+set -g test_path $PATH
+set -g fry_rubies $spec_path/dummy-rubies
+set -g fry_auto_switch 0
 
 . $spec_path/../share/fry/fry.fish
 
