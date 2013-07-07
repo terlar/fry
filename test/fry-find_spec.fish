a96c5efbddb1b954bff92046280e00c8ffc00077
Terje Larsen <terlar@gmail.com>
2013-07-07 14:02:44 +0200

    Let fuzzy match prefer the latest version

--- test/fry-find_spec.fish
+++ test/fry-find_spec.fish
@@ -2,9 +2,11 @@
 
 function setup_tank
   function fry-ls
+    echo 1.8.7-p371
+    echo 1.8.7-p374
     echo 1.9.3-p392
-    echo ruby-2.0.0-p0
     echo jruby-1.7.3
+    echo ruby-2.0.0-p0
   end
 end
 
@@ -14,6 +16,10 @@ end
 
 function it_matches_fuzzily
   test (fry-find ruby-2.0.0) = 'ruby-2.0.0-p0'
+end
+
+function it_matches_fuzzily_with_latest_version_preferred
+  test (fry-find 1.8.7) = '1.8.7-p374'
 end
 
 function it_matches_ruby_prefixed_with_version
