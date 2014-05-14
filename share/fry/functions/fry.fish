9db0879b89744478424a34a3ad910dfc0e83cacb
Terje Larsen <terlar@gmail.com>
2014-05-14 10:36:18 +0200

    fry: fix return status delegation

--- share/fry/functions/fry.fish
+++ share/fry/functions/fry.fish
@@ -11,9 +11,11 @@ function fry --description 'Fishy ruby switcher'
   set -l command $argv[1]
   set -e argv[1]
   set -l func_name "fry-$command"
+  set -l return_status 0
 
   if functions -q $func_name
     eval $func_name $argv
+    set return_status $status
   else
     if test (fry-find $command)
       fry-use $command
@@ -22,5 +24,5 @@ function fry --description 'Fishy ruby switcher'
     end
   end
 
-  return $status
+  return $return_status
 end
