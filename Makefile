0ff42d0749a43efa6ac52e771f7cb482cce311e6
Terje Larsen <terlar@gmail.com>
2014-12-14 21:37:03 +0100

    release: version 0.1.3
    - Optimized `fry rubies`, before it was slowed down by the amount of
      rubies you had installed.
    - Switch from `PATH` to `fish_user_paths`, this allows further control
      and visibility into your paths. You might not want to have the ruby
      path very first etc.
    - Add config option for `prepend` path. In case you would like to have
      identical behaviour to the previous implementation (if you are using
      `fish_user_paths` for something).
    - Introduce `fry env` that is a helper function to output a POSIX
      compatible PATH export.
    - Rubinius gems path was moved in front of the ruby path

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=fry
-VERSION=0.1.2
+VERSION=0.1.3
 AUTHOR=terlar
 URL=https://github.com/$(AUTHOR)/$(NAME)
 
--- share/fry/functions/fry-version.fish
+++ share/fry/functions/fry-version.fish
@@ -1,3 +1,3 @@
 function fry-version --description 'Display the current version of fry'
-	echo '0.1.2'
+	echo '0.1.3'
 end
